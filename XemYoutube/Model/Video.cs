ed08e26d791f59cd5faf2d94ef21fee8d3529395
anhlhd00587 <anhlhd00587@fpt.edu.vn>
2018-10-25 08:59:26 +0700

    Youtube API

---
+++ XemYoutube/Model/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XemYoutube.Model
+{
+    class Video
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Img { get; set; }
+        public DateTime? Pundate { get; set; }
+    }
+}
